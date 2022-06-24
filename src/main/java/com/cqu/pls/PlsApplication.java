c099c933680f3555146bef995c58b58994fb6a3b
wangyaohui994 <1444988446@qq.com>
2022-06-24 15:59:34 +0800

    扫描dao层

--- src/main/java/com/cqu/pls/PlsApplication.java
+++ src/main/java/com/cqu/pls/PlsApplication.java
@@ -1,9 +1,11 @@
 package com.cqu.pls;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cqu.pls.dao")
 public class PlsApplication {
 
     public static void main(String[] args) {
