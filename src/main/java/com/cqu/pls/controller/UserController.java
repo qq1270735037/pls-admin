4fb6b4e66ba3cdf3860c82926640487f24a205f4
ranhongtao <11182189+ranhongtao@user.noreply.gitee.com>
2022-07-01 10:38:54 +0800

    userController里添加统计销售额接口

--- src/main/java/com/cqu/pls/controller/UserController.java
+++ src/main/java/com/cqu/pls/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.cqu.pls.controller;
 
-import com.cqu.pls.entity.User;
-import com.cqu.pls.service.UserService;
+import com.cqu.pls.entity.*;
+import com.cqu.pls.service.*;
 import com.cqu.pls.utils.result.DataResult;
+import com.cqu.pls.vo.TransportationAndCar;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * (User)表控制层
@@ -17,11 +21,24 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("user")
 public class UserController {
+
+
+
+
+
     /**
      * 服务对象
      */
     @Resource
     private UserService userService;
+    @Resource
+    private PurchasesaleService purchasesaleService;
+    @Resource
+    private CarchangeService carchangeService;
+    @Resource
+    private TransportationinfoService transportationinfoService;
+    @Resource
+    private AdvertisementinfoService advertisementinfoService;
 
     /**
      * 分页查询
@@ -41,6 +58,62 @@ public class UserController {
      * @param id 主键
      * @return 单条数据
      */
+
+    @GetMapping("/getSalesValue")
+    public DataResult getSalesValue(){
+
+        //货物销售额
+        List<Purchasesale> purchasesaleList = purchasesaleService.queryAll();
+        int totalSales1 = 0;
+        for (Purchasesale purchasesale:
+             purchasesaleList) {
+            if(purchasesale.getPurchasesaleType().equals("售出")){
+                totalSales1+= purchasesale.getPurchasesalePrice()*purchasesale.getPurchasesaleCount();
+
+            }
+
+        }
+        //汽车无价格,丢弃
+//        Carchange carchange1 = new Carchange();
+//        List<Carchange> carchangeList = carchangeService.getByOperation(carchange1);
+//        for (Carchange carchange:
+//             carchangeList) {
+//            if(carchange.getOperation()==0){
+//            }
+//
+//        }
+
+        //
+        int totalSales2=0;
+        TransportationAndCar transportationAndCar1 = new TransportationAndCar();
+        List<TransportationAndCar> transportationAndCars1List = transportationinfoService.queryBycondition(transportationAndCar1);
+        for (TransportationAndCar transportationAndCar:
+             transportationAndCars1List) {
+            totalSales2+= Integer.parseInt(transportationAndCar.getTransportationMoney());
+        }
+
+        int totalSale3=0;
+
+
+        Advertisementinfo advertisementinfo1 = new Advertisementinfo();
+        List<Advertisementinfo> advertisementinfoList = advertisementinfoService.queryAll(advertisementinfo1);
+        for (Advertisementinfo advertisementinfo:
+             advertisementinfoList) {
+            totalSale3+=  Integer.parseInt(advertisementinfo.getAdMoney()) ;
+
+        }
+
+        int totalMoney = totalSales1+totalSales2+totalSale3;
+        return DataResult.successByDatas(totalMoney);
+
+
+
+    }
+
+
+
+
+
     @GetMapping("{id}")
     public ResponseEntity<User> queryById(@PathVariable("id") Integer id) {
         return ResponseEntity.ok(this.userService.queryById(id));
